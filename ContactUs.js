a3a57210b7981bb7e5c5a1aa8b1a057bf53ec05e
Bhavana <132038004+bhavana2907@users.noreply.github.com>
2024-11-27 21:12:07 +0530

    Create ContactUs.js

---
+++ ContactUs.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './ContactUs.css';
+const ContactUs = () => {
+ return (
+ <div className="contact">
+ <h1 align="left">Contact Us:</h1>
+ <ul>
+ <li>Phone:+91 1234567890</li>
+<br/>
+ <li>Email:Destino@gmail.com</li>
+ <br/>
+ <li>Instagram:@_Destino</li>
+ <br/>
+ <li>Facebook:@_Destino</li>
+ <br/>
+</ul>
+ </div>
+ );
+};
+export default ContactUs;
