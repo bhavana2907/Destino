1c2f32bb8d2dd1f86eb0a158a92bb0d40fb1cc36
Bhavana <132038004+bhavana2907@users.noreply.github.com>
2024-11-27 20:49:59 +0530

    Create HomePage.js

---
+++ HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Homepage.css';
+function HomePage(){
+ return (
+ <div className='home'>
+ <br/>
+ <br/>
+ <h1 className="p" align="center">Welcome to Our Travel Adventure</h1>
+ <h3 className="p" align="center">Discover amazing destinations, create unforgettable
+memories.</h3>
+ </div>
+ );
+};
+export default HomePage; 
