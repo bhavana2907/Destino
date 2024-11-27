13052008289c816c35f67a922e26ce65ac27b420
Bhavana <132038004+bhavana2907@users.noreply.github.com>
2024-11-27 20:54:30 +0530

    Create TajMahal.js

---
+++ TajMahal.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { BrowserRouter as Router} from 'react-router-dom';
+const TajMahal = () => {
+ return (
+ <Router>
+ <div>
+ <h2>Taj Mahal</h2>
+ <h3>Location:Agra,Uttar Pradesh</h3>
+ <h3>Nearby Hotels:</h3>
+ <ul>
+ <li>The Oberoi Amarvilas</li>
+ <li>Trident, Agra</li>
+ <li>Hotel Taj Resorts</li>
+ </ul>
+ <h3>Distance from Main Stations:</h3>
+ <ul>
+ <li>Agra Cantt Railway Station: Approximately 6 km</li>
+ <li>Agra Fort Railway Station: Approximately 4 km</li>
+ </ul>
+ <h3>T<li>Agra Cantt Railway Station: Approximately 6 km</li>
+ <li>Agra Fort Railway Station: Approximately 4 km</li>
+ </ul>
+ <h3>Transportation:</h3>
+ <ul>
+ <li>Taxis and auto-rickshaws are available for local transportation.</li>
+ <li>Agra is well-connected by road, and buses are available from major cities.</li>
+ <li>Agra also has its own airport, the Pandit Deen Dayal Upadhyay Airport, with
+domestic flights.</li>
+</ul>
+ <h3>Minimum Budget</h3>
+ <ul>
+ <li>Accommodation: ₹2000 - ₹8000 per day</li>
+ <li>Transportation: ₹500 - ₹2000</li>
+ <li>Meals and other expenses: ₹1000 - ₹3000 per day</li>
+ </ul>
+ <h3>Nearest Tourist Spots:</h3>
+ <ul>
+ <li>Agra Fort</li>
+ <li>Jama Masjid
+ <li>Mehtab Bagh</li>
+ </li>
+ </ul>
+ </div>
+</Router>
+ );
+};
