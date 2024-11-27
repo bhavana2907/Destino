5698db17053187154c861728d7f75782dd5ced19
Bhavana <132038004+bhavana2907@users.noreply.github.com>
2024-11-27 20:58:16 +0530

    Create RedFort.js

---
+++ RedFort.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { BrowserRouter as Router} from 'react-router-dom';
+const RedFort = () => {
+ return (
+ <Router>
+ <div>
+ <h2>Red Fort</h2>
+ <h3>Location:Old Delhi,India</h3>
+ <h3>Nearby Hotels:</h3>
+<h3>Location:Old Delhi,India</h3>
+ <h3>Nearby Hotels:</h3>
+ <ul>
+ <li>The Lalit</li>
+ <li>Le Meridien</li>
+ <li>Maidens Hotel</li>
+ </ul>
+ <h3>Distance from Main Stations:</h3>
+ <ul>
+ <li>New Delhi Railway Station: Approximately 4 km.</li>
+ </ul>
+ <h3>Transportation:</h3>
+ <ul>
+ <li>Well-connected by metro, buses, taxis, and cycle rickshaws.</li>
+ </ul>
+ <h3>Minimum Budget</h3>
+ <ul>
+ <li>Accommodation: ₹2000 - ₹7000 per day</li>
+ <li>Transportation: ₹500 - ₹1500</li>
+ <li>Meals and other expenses: ₹1000 - ₹2000 per day</li>
+ </ul>
+<li>Chandni Chowk</li>
+ <li>Shanti Van and Raj Ghat</li>
+ <li>Fatehpuri Masjid</li>
+ </ul>
+</div>
+</Router>
+ );
+};
+export default RedFort;
